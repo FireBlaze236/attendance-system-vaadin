09f8912f0472a028c803264c4daedc306159a42e
spiritinchains <22220607+spiritinchains@users.noreply.github.com>
2021-01-04 00:29:53 +0600

    Implement interval-based Photo snapshots

--- src/main/java/com/packagename/prototype1/VideoComponent.java
+++ src/main/java/com/packagename/prototype1/VideoComponent.java
@@ -1,11 +1,22 @@
 package com.packagename.prototype1;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.DomEvent;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.shared.Registration;
 
 @Tag("div")
 public class VideoComponent extends Component {
+    @DomEvent("snapshot")
+    static public class ImageSnapEvent extends ComponentEvent<VideoComponent> {
+        public ImageSnapEvent(VideoComponent source, boolean fromClient) {
+            super(source, fromClient);
+        }
+    }
+
     Element videoElement = new Element("video");
     Element canvasElement = new Element("canvas");
 
@@ -15,5 +26,9 @@ public class VideoComponent extends Component {
             videoElement,
             canvasElement
         );
+    }
+    public Registration addSnapshotListener(
+         ComponentEventListener<ImageSnapEvent> listener) {
+        return addListener(ImageSnapEvent.class, listener);
     }
 }
--- src/main/java/com/packagename/prototype1/views/CameraTestView.java
+++ src/main/java/com/packagename/prototype1/views/CameraTestView.java
@@ -1,38 +1,19 @@
 package com.packagename.prototype1.views;
 
 import com.packagename.prototype1.VideoComponent;
-import com.packagename.prototype1.backend.facerec.FaceRecognizerModule;
-import com.packagename.prototype1.backend.model.UserData;
-import com.packagename.prototype1.backend.repository.UserRepository;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.PendingJavaScriptResult;
-import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.router.Route;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.io.IOException;
 
 @Route("camera")
 public class CameraTestView extends VerticalLayout {
-
-    @Autowired
-    UserRepository userRepository;
-
     private Button cameraButton = new Button("Access Camera");
-    private Button snapButton = new Button("Predict");
-    private Button trainButton = new Button("Train");
-    private IntegerField idField = new IntegerField("Id: ");
+    private Button snapButton = new Button("Snap");
     private VideoComponent vComponent = new VideoComponent();
-    private int id = 101;
-    private Text tm = new Text("");
-
     public CameraTestView() {
-        //FaceRecognizerModule.init();
         /* 
          * JavaScript Snippets:
          * jsinit = initializes media device and resizes canvas
@@ -54,20 +35,13 @@ public class CameraTestView extends VerticalLayout {
         add(cameraButton);
         cameraButton.addClickListener(cl -> {
             add(snapButton);
-            add(idField);
-            add(trainButton);
-            add(tm);
             add(vComponent);
-
             UI.getCurrent().getPage().executeJs(
                 jsinit,
                 vComponent.getElement().getChild(0),
                 vComponent.getElement().getChild(1)
             );
-            //fr.train();
-            //System.out.println("Model Trained");
         });
-
 
         snapButton.addClickListener(c -> {
             PendingJavaScriptResult res = UI.getCurrent().getPage().executeJs(
@@ -77,65 +51,8 @@ public class CameraTestView extends VerticalLayout {
             );
             res.then(String.class, dataURL -> {
                 Image snappedImage = new Image(dataURL, "snapshot");
-                //add (snappedImage);
-                String str = dataURL;
-                int level = -1;
-                try {
-                    level = FaceRecognizerModule.compareimage (str);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(level);
-                //new code
-                if(level == -1 || !userRepository.findById((long)level).isPresent())
-                {
-                    tm.setText("Face not recognized");
-                }
-                else
-                {
-                    UserData user = userRepository.findById((long) level).get();
-                    tm.setText(user.getUsername());
-                }
+                add(snappedImage);
             });
         });
-
-        trainButton.addClickListener(c -> {
-            tm.setText("Trainining Initiated!");
-
-            String username = SecurityContextHolder.getContext().getAuthentication().getName();
-            if (userRepository.findByUsername(username).isPresent())
-            {
-                UserData user = userRepository.findByUsername(username).get();
-                id = user.getId().intValue();
-            }
-            else
-            {
-                id = -1;
-            }
-
-            for (int i = 0; i < 6; i++) {
-                PendingJavaScriptResult res = UI.getCurrent().getPage().executeJs(
-                        jstakepic,
-                        vComponent.getElement().getChild(0),
-                        vComponent.getElement().getChild(1)
-                );
-                int temp = i;
-
-
-                //STRING ARRAY use korbo
-                res.then(String.class, dataURL -> {
-                    String str = dataURL;
-                    try {
-                        FaceRecognizerModule.take_in_Vector(str, id, temp);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    tm.setText("Training Done");
-                });
-
-            }
-
-        });
-
     }
 }

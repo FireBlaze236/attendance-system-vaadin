5d7c63e1d3ee4abd21ba6477c49de15a09c26c94
spiritinchains <22220607+spiritinchains@users.noreply.github.com>
2021-01-05 04:31:30 +0600

    no name

--- src/main/java/com/packagename/prototype1/views/GiveAttendanceView.java
+++ src/main/java/com/packagename/prototype1/views/GiveAttendanceView.java
@@ -1,5 +1,7 @@
 package com.packagename.prototype1.views;
 
+import com.packagename.prototype1.FaceDetector;
+import com.packagename.prototype1.VideoComponent;
 import com.packagename.prototype1.backend.model.AttendanceData;
 import com.packagename.prototype1.backend.model.SessionData;
 import com.packagename.prototype1.backend.repository.AttendanceDataRepository;
@@ -8,6 +10,7 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -17,6 +20,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 @Route("give")
 public class GiveAttendanceView extends VerticalLayout {
@@ -25,6 +30,10 @@ public class GiveAttendanceView extends VerticalLayout {
     @Autowired
     AttendanceDataRepository attendanceDataRepository;
 
+    private String current_user = new String();
+    private SessionData sessionData = new SessionData();
+
+    private H1 pageHeader = new H1("Give Attendance");
     private TextField idField = new TextField("ID : ");
     private TextField nameField = new TextField("Name : ");
     private TextField codeField = new TextField("Code : ");
@@ -32,13 +41,47 @@ public class GiveAttendanceView extends VerticalLayout {
     private Button backButton = new Button("Back");
     //private Anchor viewLink = new Anchor("grid","View Attendance");
 
+    AttendanceData attendanceData = new AttendanceData();
+
+    private VideoComponent videoComponent = new VideoComponent();
+    private H4 attendanceNotice = new H4(
+        "Your Attendance is being recorded. Please keep this tab open."
+    );
+
+    private FaceDetector fd = new FaceDetector();
+
+    private CompletableFuture<String> imageURLData = new CompletableFuture<>();
+    private Thread worker = new Thread() {
+        public void run() {
+            while (recordInProgress) {
+                boolean verified = false;
+                try {
+                    verified = fd.detect(imageURLData.get());
+                    System.out.println("here");
+                }
+                catch (Exception e) {
+                    System.out.println("llll");
+                }
+                if (verified) {
+                    //attendanceData = attendanceDataRepository.findByUsernameAndSessionData(current_user, sessionData).get();
+                    //int cscore = attendanceData.getScore();
+                    //attendanceData.setScore(cscore + 5);
+                    //attendanceDataRepository.save(attendanceData);
+                    System.out.println("score incremented");
+                }
+            }
+        }
+    };
+
+    private boolean recordInProgress = false;
+
     public GiveAttendanceView()
     {
         setJustifyContentMode(JustifyContentMode.CENTER);
         setAlignItems(Alignment.CENTER);
 
         submit_button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        add(new H1("Give Attendance"), idField, nameField, codeField, submit_button, backButton);
+        add(pageHeader, idField, nameField, codeField, submit_button, backButton);
 
         submit_button.addClickListener(clicked ->
         {
@@ -62,35 +105,72 @@ public class GiveAttendanceView extends VerticalLayout {
             }
             if(LocalDateTime.now().isBefore(sessionData.getSessionEndTime().toLocalDateTime()) && LocalDateTime.now().isAfter(sessionData.getSessionStartTime().toLocalDateTime()))
             {
+                // Start Recording
+                remove(idField, nameField, codeField, submit_button, backButton);
+                add(attendanceNotice);
 
                 codeField.setInvalid(false);
-                AttendanceData attendanceData = new AttendanceData();
-                attendanceData.setStudentId(studentId);
-                attendanceData.setStudentName(studentName);
-                attendanceData.setSessionData(sessionData);
-
                 //user who gave the attendance
-                String current_user = SecurityContextHolder.getContext().getAuthentication().getName();
-                attendanceData.setUsername(current_user);
+                current_user = SecurityContextHolder.getContext().getAuthentication().getName();
                 //user's ip address
                 String current_user_ip = VaadinSession.getCurrent().getBrowser().getAddress();
-                attendanceData.setUserIp(current_user_ip);
 
-                if(attendanceDataRepository.findByUsername(current_user).isPresent())
+                // if entry exists, update; otherwise add
+                
+                Optional<AttendanceData> ad = attendanceDataRepository.findByUsernameAndSessionData(current_user, sessionData);
+
+                if(ad.isEmpty())
+                {
+                    // add
+                    attendanceData.setStudentId(studentId);
+                    attendanceData.setStudentName(studentName);
+                    attendanceData.setSessionData(sessionData);
+    
+                    attendanceData.setUsername(current_user);
+                    attendanceData.setUserIp(current_user_ip);
+
+                    attendanceData.setScore(0);
+                }
+                else {
+                    // update
+                    attendanceData = ad.get();
+                }
+                
+                /*
+                if(attendanceDataRepository.findByUsernameAndSessionData(current_user, sessionData).isEmpty())
                 {
                     codeField.setInvalid(true);
                     codeField.setErrorMessage("You have already given your attendance in this session.");
                     return;
                 }
+                */
 
+                recordInProgress = true;
                 attendanceDataRepository.save(attendanceData);
+
+                add(videoComponent);
+
+                videoComponent.addSnapshotListener(ev -> {
+                    imageURLData = videoComponent.getElement().getChild(1).executeJs(
+                        "var context = this.getContext('2d');" +
+                        "context.drawImage($0, 0, 0, this.width, this.height);" +
+                        "return this.toDataURL();",
+                        videoComponent.getElement().getChild(0)
+                    ).toCompletableFuture(String.class);
+                });
+
+                videoComponent.startIntervalSnap(2000);
+                worker.start();
+
                 //Notfiy the user
                 Notification notification = new Notification("Attendance recorded!", 1500, Notification.Position.TOP_CENTER);
                 notification.open();
+                /*
                 notification.addOpenedChangeListener(openedChangeEvent->
                 {
                     UI.getCurrent().navigate("");
                 });
+                */
             }
             else
             {

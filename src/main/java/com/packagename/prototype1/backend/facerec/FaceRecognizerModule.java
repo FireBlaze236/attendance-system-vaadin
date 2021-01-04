82fbfd839180e8a43ed8bdc05dbe52846fed8c04
M. K. Bashar <bashar.onion@gmail.com>
2021-01-05 02:55:39 +0600

    From Taufiq

--- src/main/java/com/packagename/prototype1/backend/facerec/FaceRecognizerModule.java
+++ src/main/java/com/packagename/prototype1/backend/facerec/FaceRecognizerModule.java
@@ -1,8 +1,9 @@
 package com.packagename.prototype1.backend.facerec;
 
-import nu.pattern.OpenCV;
 import org.bytedeco.javacpp.DoublePointer;
 import org.bytedeco.javacpp.IntPointer;
+import org.bytedeco.javacv.Java2DFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.opencv.opencv_core.MatVector;
 import org.bytedeco.opencv.opencv_face.FaceRecognizer;
 import org.bytedeco.opencv.opencv_face.LBPHFaceRecognizer;
@@ -11,34 +12,28 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import java.io.IOException;
 import java.nio.IntBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Base64;
 
 import static org.bytedeco.opencv.global.opencv_imgcodecs.IMREAD_UNCHANGED;
 import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
 import static org.opencv.core.CvType.CV_32SC1;
 import static org.opencv.imgproc.Imgproc.*;
+import nu.pattern.OpenCV;
 
 public class FaceRecognizerModule {
 
-    public static FaceRecognizer faceRecognizer = LBPHFaceRecognizer.create();
-    public static MatVector images = new MatVector(1);
-    //List<org.bytedeco.opencv.opencv_core.Mat> images = new ArrayList<org.bytedeco.opencv.opencv_core.Mat>();
-    public static org.bytedeco.opencv.opencv_core.Mat labels = new org.bytedeco.opencv.opencv_core.Mat(1, 1, CV_32SC1);
-    public static IntBuffer labelsBuf = labels.createBuffer();
-    //My code
-    public static void init()
-    {
-        OpenCV.loadLocally();
-        faceRecognizer.read("mymodel.xml");
-    }
-
-
-    public static int compareimage (String str) throws IOException {
+    public int compareimage (String str) throws IOException {
         OpenCV.loadLocally();
         CascadeClassifier faceDetector = new CascadeClassifier("haarcascade_frontalface_alt.xml");
-        //FaceRecognizer faceRecognizer = LBPHFaceRecognizer.create();
+        FaceRecognizer faceRecognizer = LBPHFaceRecognizer.create();
         byte[] data = Base64.getDecoder().decode(
                 str.substring(str.indexOf(",") + 1).getBytes()
         );
@@ -63,20 +58,21 @@ public class FaceRecognizerModule {
 
             org.bytedeco.opencv.opencv_core.Mat bufface = new org.bytedeco.opencv.opencv_core.Mat() { { address = faceCaptured.getNativeObjAddr(); } };
 
-            //faceRecognizer.read("mymodel.xml");
-
+            faceRecognizer.read("mymodel.xml");
             faceRecognizer.predict (bufface, label, confidence);
 
             prediction = label.get(0);
-            double x = confidence.get(0);
-            System.out.println(x);
         }
         return prediction;
     }
 
-    public static void take_in_Vector(String str,int id, int count) throws IOException {
+    public static MatVector images = new MatVector(10);
+    public static org.bytedeco.opencv.opencv_core.Mat labels = new org.bytedeco.opencv.opencv_core.Mat(10, 1, CV_32SC1);
+    public static IntBuffer labelsBuf = labels.createBuffer();
 
+    public static void take_in_Vector(String str,int id, int count) throws IOException {
         OpenCV.loadLocally();
+        //String str = new String(Files.readAllBytes(Paths.get("Predict_image//data.txt")));
         CascadeClassifier faceDetector = new CascadeClassifier("haarcascade_frontalface_alt.xml");
 
         byte[] data = Base64.getDecoder().decode(
@@ -88,6 +84,8 @@ public class FaceRecognizerModule {
         faceDetector.detectMultiScale(imgFrame, faceDetections);
 
         for (Rect rect : faceDetections.toArray()) {
+            System.out.println("fff");
+
             rectangle(imgFrame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
                     new Scalar(0, 0, 0));
             Rect face = new Rect (rect.x, rect.y, rect.width, rect.height);
@@ -98,12 +96,15 @@ public class FaceRecognizerModule {
 
             org.bytedeco.opencv.opencv_core.Mat mat2 = new org.bytedeco.opencv.opencv_core.Mat() { { address = faceCaptured.getNativeObjAddr(); } };
 
-            imwrite(String.format("%d+input.jpg", count), mat2);
-            images.put(count,mat2);
-            labelsBuf.put(count,id);
-            break;
+            images.put((long) count, mat2);
+            labelsBuf.put(count, id);
         }
-        faceRecognizer.update(images, labels);
-        faceRecognizer.write("mymodel.xml");
+    }
+
+    public static void train()
+    {
+        FaceRecognizer faceRecognizer = LBPHFaceRecognizer.create();
+        faceRecognizer.train(images, labels);
+        faceRecognizer.save("mymodel.xml");
     }
 }
--- src/main/java/com/packagename/prototype1/views/CameraTestView.java
+++ src/main/java/com/packagename/prototype1/views/CameraTestView.java
@@ -2,37 +2,32 @@ package com.packagename.prototype1.views;
 
 import com.packagename.prototype1.VideoComponent;
 import com.packagename.prototype1.backend.facerec.FaceRecognizerModule;
-import com.packagename.prototype1.backend.model.UserData;
-import com.packagename.prototype1.backend.repository.UserRepository;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.PendingJavaScriptResult;
-import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.bytedeco.javacpp.BytePointer;
 
+import javax.xml.bind.DatatypeConverter;
+import java.io.File;
 import java.io.IOException;
+
+import static com.packagename.prototype1.backend.facerec.FaceRecognizerModule.train;
 
 @Route("camera")
 public class CameraTestView extends VerticalLayout {
-
-    @Autowired
-    UserRepository userRepository;
-
     private Button cameraButton = new Button("Access Camera");
     private Button snapButton = new Button("Predict");
     private Button trainButton = new Button("Train");
-    private IntegerField idField = new IntegerField("Id: ");
     private VideoComponent vComponent = new VideoComponent();
     private int id = 101;
-    private Text tm = new Text("");
+    private Text tm = new Text("Training Done");
 
     public CameraTestView() {
-        //FaceRecognizerModule.init();
         /* 
          * JavaScript Snippets:
          * jsinit = initializes media device and resizes canvas
@@ -54,11 +49,8 @@ public class CameraTestView extends VerticalLayout {
         add(cameraButton);
         cameraButton.addClickListener(cl -> {
             add(snapButton);
-            add(idField);
             add(trainButton);
-            add(tm);
             add(vComponent);
-
             UI.getCurrent().getPage().executeJs(
                 jsinit,
                 vComponent.getElement().getChild(0),
@@ -70,6 +62,7 @@ public class CameraTestView extends VerticalLayout {
 
 
         snapButton.addClickListener(c -> {
+            FaceRecognizerModule fr = new FaceRecognizerModule();
             PendingJavaScriptResult res = UI.getCurrent().getPage().executeJs(
                 jstakepic,
                 vComponent.getElement().getChild(0),
@@ -81,61 +74,37 @@ public class CameraTestView extends VerticalLayout {
                 String str = dataURL;
                 int level = -1;
                 try {
-                    level = FaceRecognizerModule.compareimage (str);
+                    level = fr.compareimage (str);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 System.out.println(level);
-                //new code
-                if(level == -1 || !userRepository.findById((long)level).isPresent())
-                {
-                    tm.setText("Face not recognized");
-                }
-                else
-                {
-                    UserData user = userRepository.findById((long) level).get();
-                    tm.setText(user.getUsername());
-                }
+                Text tf = new Text(String.valueOf(level));
+                add (tf);
             });
         });
 
         trainButton.addClickListener(c -> {
-            tm.setText("Trainining Initiated!");
-
-            String username = SecurityContextHolder.getContext().getAuthentication().getName();
-            if (userRepository.findByUsername(username).isPresent())
-            {
-                UserData user = userRepository.findByUsername(username).get();
-                id = user.getId().intValue();
-            }
-            else
-            {
-                id = -1;
-            }
-
-            for (int i = 0; i < 6; i++) {
+            FaceRecognizerModule fr = new FaceRecognizerModule();
+            for (int i = 0; i < 10; i++) {
                 PendingJavaScriptResult res = UI.getCurrent().getPage().executeJs(
                         jstakepic,
                         vComponent.getElement().getChild(0),
                         vComponent.getElement().getChild(1)
                 );
                 int temp = i;
-
-
-                //STRING ARRAY use korbo
                 res.then(String.class, dataURL -> {
                     String str = dataURL;
                     try {
-                        FaceRecognizerModule.take_in_Vector(str, id, temp);
+                        fr.take_in_Vector(str, id, temp);
                     } catch (IOException e) {
-                        e.printStackTrace();
+
                     }
-                    tm.setText("Training Done");
                 });
-
             }
-
+            train();
+            id++;
+            add(tm);
         });
-
     }
 }

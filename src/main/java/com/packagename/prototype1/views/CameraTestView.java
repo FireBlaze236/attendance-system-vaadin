573aca6e1292d835c6983feac25d18447efe9314
M. K. Bashar <bashar.onion@gmail.com>
2021-01-05 02:59:17 +0600

    Revert "From Taufiq"
    This reverts commit 82fbfd83

--- src/main/java/com/packagename/prototype1/views/CameraTestView.java
+++ src/main/java/com/packagename/prototype1/views/CameraTestView.java
@@ -2,32 +2,37 @@ package com.packagename.prototype1.views;
 
 import com.packagename.prototype1.VideoComponent;
 import com.packagename.prototype1.backend.facerec.FaceRecognizerModule;
+import com.packagename.prototype1.backend.model.UserData;
+import com.packagename.prototype1.backend.repository.UserRepository;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.PendingJavaScriptResult;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.router.Route;
-import org.bytedeco.javacpp.BytePointer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-import javax.xml.bind.DatatypeConverter;
-import java.io.File;
 import java.io.IOException;
-
-import static com.packagename.prototype1.backend.facerec.FaceRecognizerModule.train;
 
 @Route("camera")
 public class CameraTestView extends VerticalLayout {
+
+    @Autowired
+    UserRepository userRepository;
+
     private Button cameraButton = new Button("Access Camera");
     private Button snapButton = new Button("Predict");
     private Button trainButton = new Button("Train");
+    private IntegerField idField = new IntegerField("Id: ");
     private VideoComponent vComponent = new VideoComponent();
     private int id = 101;
-    private Text tm = new Text("Training Done");
+    private Text tm = new Text("");
 
     public CameraTestView() {
+        //FaceRecognizerModule.init();
         /* 
          * JavaScript Snippets:
          * jsinit = initializes media device and resizes canvas
@@ -49,8 +54,11 @@ public class CameraTestView extends VerticalLayout {
         add(cameraButton);
         cameraButton.addClickListener(cl -> {
             add(snapButton);
+            add(idField);
             add(trainButton);
+            add(tm);
             add(vComponent);
+
             UI.getCurrent().getPage().executeJs(
                 jsinit,
                 vComponent.getElement().getChild(0),
@@ -62,7 +70,6 @@ public class CameraTestView extends VerticalLayout {
 
 
         snapButton.addClickListener(c -> {
-            FaceRecognizerModule fr = new FaceRecognizerModule();
             PendingJavaScriptResult res = UI.getCurrent().getPage().executeJs(
                 jstakepic,
                 vComponent.getElement().getChild(0),
@@ -74,37 +81,61 @@ public class CameraTestView extends VerticalLayout {
                 String str = dataURL;
                 int level = -1;
                 try {
-                    level = fr.compareimage (str);
+                    level = FaceRecognizerModule.compareimage (str);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 System.out.println(level);
-                Text tf = new Text(String.valueOf(level));
-                add (tf);
+                //new code
+                if(level == -1 || !userRepository.findById((long)level).isPresent())
+                {
+                    tm.setText("Face not recognized");
+                }
+                else
+                {
+                    UserData user = userRepository.findById((long) level).get();
+                    tm.setText(user.getUsername());
+                }
             });
         });
 
         trainButton.addClickListener(c -> {
-            FaceRecognizerModule fr = new FaceRecognizerModule();
-            for (int i = 0; i < 10; i++) {
+            tm.setText("Trainining Initiated!");
+
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            if (userRepository.findByUsername(username).isPresent())
+            {
+                UserData user = userRepository.findByUsername(username).get();
+                id = user.getId().intValue();
+            }
+            else
+            {
+                id = -1;
+            }
+
+            for (int i = 0; i < 6; i++) {
                 PendingJavaScriptResult res = UI.getCurrent().getPage().executeJs(
                         jstakepic,
                         vComponent.getElement().getChild(0),
                         vComponent.getElement().getChild(1)
                 );
                 int temp = i;
+
+
+                //STRING ARRAY use korbo
                 res.then(String.class, dataURL -> {
                     String str = dataURL;
                     try {
-                        fr.take_in_Vector(str, id, temp);
+                        FaceRecognizerModule.take_in_Vector(str, id, temp);
                     } catch (IOException e) {
-
+                        e.printStackTrace();
                     }
+                    tm.setText("Training Done");
                 });
+
             }
-            train();
-            id++;
-            add(tm);
+
         });
+
     }
 }

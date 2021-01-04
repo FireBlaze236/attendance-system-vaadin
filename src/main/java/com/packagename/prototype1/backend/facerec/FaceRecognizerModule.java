190dd21ef3bb17bd2608cde069d0662a0a8fe361
Taufiqul Alam <saminps3@gmail.com>
2021-01-05 01:29:11 +0600

    In memory

--- src/main/java/com/packagename/prototype1/backend/facerec/FaceRecognizerModule.java
+++ src/main/java/com/packagename/prototype1/backend/facerec/FaceRecognizerModule.java
@@ -1,9 +1,8 @@
 package com.packagename.prototype1.backend.facerec;
 
+import nu.pattern.OpenCV;
 import org.bytedeco.javacpp.DoublePointer;
 import org.bytedeco.javacpp.IntPointer;
-import org.bytedeco.javacv.Java2DFrameConverter;
-import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.opencv.opencv_core.MatVector;
 import org.bytedeco.opencv.opencv_face.FaceRecognizer;
 import org.bytedeco.opencv.opencv_face.LBPHFaceRecognizer;
@@ -12,10 +11,6 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.WritableRaster;
 import java.io.IOException;
 import java.nio.IntBuffer;
 import java.util.Base64;
@@ -24,14 +19,21 @@ import static org.bytedeco.opencv.global.opencv_imgcodecs.IMREAD_UNCHANGED;
 import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
 import static org.opencv.core.CvType.CV_32SC1;
 import static org.opencv.imgproc.Imgproc.*;
-import nu.pattern.OpenCV;
 
 public class FaceRecognizerModule {
 
-    public int compareimage (String str) throws IOException {
+    public static FaceRecognizer faceRecognizer = LBPHFaceRecognizer.create();
+    public static MatVector images = new MatVector(1);
+    //List<org.bytedeco.opencv.opencv_core.Mat> images = new ArrayList<org.bytedeco.opencv.opencv_core.Mat>();
+    public static org.bytedeco.opencv.opencv_core.Mat labels = new org.bytedeco.opencv.opencv_core.Mat(1, 1, CV_32SC1);
+    public static IntBuffer labelsBuf = labels.createBuffer();
+    //My code
+
+
+    public static int compareimage (String str) throws IOException {
         OpenCV.loadLocally();
         CascadeClassifier faceDetector = new CascadeClassifier("haarcascade_frontalface_alt.xml");
-        FaceRecognizer faceRecognizer = LBPHFaceRecognizer.create();
+        //FaceRecognizer faceRecognizer = LBPHFaceRecognizer.create();
         byte[] data = Base64.getDecoder().decode(
                 str.substring(str.indexOf(",") + 1).getBytes()
         );
@@ -54,33 +56,21 @@ public class FaceRecognizerModule {
             IntPointer label = new IntPointer(1);
             DoublePointer confidence = new DoublePointer(1);
 
-            int type = 0;
-            if (faceCaptured.channels() == 1) {
-                type = BufferedImage.TYPE_BYTE_GRAY;
-            } else if (faceCaptured.channels() == 3) {
-                type = BufferedImage.TYPE_3BYTE_BGR;
-            }
-            BufferedImage image = new BufferedImage(faceCaptured.width() ,faceCaptured.height(), type);
-            WritableRaster raster = image.getRaster();
-            DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
-            byte[] data2 = dataBuffer.getData();
-            faceCaptured.get(0, 0, data2);
+            org.bytedeco.opencv.opencv_core.Mat bufface = new org.bytedeco.opencv.opencv_core.Mat() { { address = faceCaptured.getNativeObjAddr(); } };
 
-            OpenCVFrameConverter.ToMat cv = new OpenCVFrameConverter.ToMat();
-            org.bytedeco.opencv.opencv_core.Mat bufface =  cv.convertToMat(new Java2DFrameConverter().convert(image));
+            //faceRecognizer.read("mymodel.xml");
 
-            faceRecognizer.read("mymodel.xml");
             faceRecognizer.predict (bufface, label, confidence);
 
             prediction = label.get(0);
+            double x = confidence.get(0);
+            System.out.println(x);
         }
         return prediction;
     }
 
-    public void take_in_Vector(String str,int id, int count) throws IOException {
-        MatVector images = new MatVector(1);
-        org.bytedeco.opencv.opencv_core.Mat labels = new org.bytedeco.opencv.opencv_core.Mat(1, 1, CV_32SC1);
-        IntBuffer labelsBuf = labels.createBuffer();
+    public static void take_in_Vector(String str,int id, int count) throws IOException {
+
         OpenCV.loadLocally();
         CascadeClassifier faceDetector = new CascadeClassifier("haarcascade_frontalface_alt.xml");
 
@@ -93,8 +83,6 @@ public class FaceRecognizerModule {
         faceDetector.detectMultiScale(imgFrame, faceDetections);
 
         for (Rect rect : faceDetections.toArray()) {
-            //System.out.println("fff");
-
             rectangle(imgFrame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
                     new Scalar(0, 0, 0));
             Rect face = new Rect (rect.x, rect.y, rect.width, rect.height);
@@ -105,12 +93,12 @@ public class FaceRecognizerModule {
 
             org.bytedeco.opencv.opencv_core.Mat mat2 = new org.bytedeco.opencv.opencv_core.Mat() { { address = faceCaptured.getNativeObjAddr(); } };
 
-            //imwrite(String.format("%d+input.jpg", count), mat2);
-            images.put(count, mat2);
-            labelsBuf.put(count, id);
+            imwrite(String.format("%d+input.jpg", count), mat2);
+            images.put(count,mat2);
+            labelsBuf.put(count,id);
+            break;
         }
-        FaceRecognizer faceRecognizer = LBPHFaceRecognizer.create();
-        faceRecognizer.train(images, labels);
-        faceRecognizer.save("mymodel.xml");
+        faceRecognizer.update(images, labels);
+        faceRecognizer.write("mymodel.xml");
     }
 }
